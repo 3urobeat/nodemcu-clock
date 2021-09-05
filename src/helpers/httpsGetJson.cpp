1bff2d83da50109873cf658022d964fe38ca07a7
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2021-09-05 16:15:00 +0200

    Added httpGetJson helper func to also support http. Added debugAndFormat helper func to format http and https request results and return debug information if enabled.

--- src/helpers/httpsGetJson.cpp
+++ src/helpers/httpsGetJson.cpp
@@ -4,28 +4,74 @@
 
 #include "helpers.h"
 
-
-DynamicJsonDocument httpsGetJson(String url)
+DynamicJsonDocument debugAndFormat(String url, int port, int httpCode, String jsondata)
 {
-    WiFiClientSecure client;
-    HTTPClient http;
+    //Set to true for debugging response in serial monitor
+    bool printDebug = false;
 
-    client.setInsecure(); //ignore handshake stuff, we are only getting the current time
-    client.connect(url, 443);
-
-    http.begin(client, url);
-
-    String jsondata;
-
-    if (http.GET() == HTTP_CODE_OK)
-    {    
-        jsondata = http.getString();
-    } else {
-        jsondata = "{ \"error\": \"http error\" }"; //tbh idk what to write here
+    if (printDebug)
+    {
+        Serial.begin(9600);
+        Serial.println("\nPinging \'" + url + "\' on port \'" + (String) port + "\'...");
+        Serial.println("Result (" + (String) httpCode + "):");
+        Serial.println(jsondata);
     }
 
     DynamicJsonDocument doc(1024);
     deserializeJson(doc, jsondata);
 
     return doc;
+}
+
+DynamicJsonDocument httpGetJson(String url)
+{
+    WiFiClient       client;
+    HTTPClient       http;
+    String           jsondata;
+
+    int port = 80;
+
+    client.connect(url, port);
+    http.begin(client, url);
+
+    int httpCode = http.GET();
+
+    if (httpCode == HTTP_CODE_OK)
+    {    
+        jsondata = http.getString();
+    } else {
+        jsondata = "{ \"error\": \"http error: " + (String) httpCode + "\"}"; //tbh idk what to write here
+    }
+
+    http.end(); //Close connection
+    client.stop();
+
+    return debugAndFormat(url, port, httpCode, jsondata);
+}
+
+DynamicJsonDocument httpsGetJson(String url)
+{
+    WiFiClientSecure client;
+    HTTPClient       http;
+    String           jsondata;
+
+    int port = 443;
+
+    client.setInsecure(); //ignore handshake stuff, we are only getting simple stuff
+    client.connect(url, port);
+    http.begin(client, url);
+
+    int httpCode = http.GET();
+
+    if (httpCode == HTTP_CODE_OK)
+    {    
+        jsondata = http.getString();
+    } else {
+        jsondata = "{ \"error\": \"https error: " + (String) httpCode + "\"}"; //tbh idk what to write here
+    }
+
+    http.end(); //Close connection
+    client.stop();
+
+    return debugAndFormat(url, port, httpCode, jsondata);
 }
