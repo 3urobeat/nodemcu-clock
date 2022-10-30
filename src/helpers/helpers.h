e938bc2e1534a44ead7f916dd1f9f1ad6515d75f
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-10-30 18:51:38 +0100

    Organized helper exports to file order

--- src/helpers/helpers.h
+++ src/helpers/helpers.h
@@ -4,7 +4,7 @@
  * Created Date: 30.08.2021 11:19:00
  * Author: 3urobeat
  * 
- * Last Modified: 26.10.2022 13:51:13
+ * Last Modified: 30.10.2022 18:49:44
  * Modified By: 3urobeat
  * 
  * Copyright (c) 2021 3urobeat <https://github.com/HerrEurobeat>
@@ -23,11 +23,15 @@
 #include "main.h"
 
 void getLocation(const char *openweathermaptoken, char *lat, char *lon, char *city, char *country, int *timeoffset);
-ESP8266WiFiClass initWifi(const char *wifiSSID[], const char *wifiPW[], size_t ssidamount, int maxcol, int row);
+
 void httpGetJson(const char *url, DynamicJsonDocument *doc, StaticJsonDocument<128> filter);
 void httpsGetJson(const char *url, DynamicJsonDocument *doc, StaticJsonDocument<128> filter);
+
+ESP8266WiFiClass initWifi(const char *wifiSSID[], const char *wifiPW[], size_t ssidamount, int maxcol, int row);
+
 char *formatInt(char *buf, int value);
-void getDate(char *dest, NTPClient timeClient, int timeoffset, const char *dateformat);
-void getTime(char *dest, NTPClient timeClient, int timeoffset, const char *timeformat);
 void strrpl(char *src, const char *oldchars, const char *newchars);
 char *mystrcat(char* dest, const char* src);
+
+void getDate(char *dest, NTPClient timeClient, int timeoffset, const char *dateformat);
+void getTime(char *dest, NTPClient timeClient, int timeoffset, const char *timeformat);
