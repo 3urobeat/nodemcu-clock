2ddef5ca17b8c12f6b5562e71684e42984efffa2
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-12-30 21:56:01 +0100

    Accept defeat and do it the lame way

--- src/setupMode/setupMode.h
+++ src/setupMode/setupMode.h
@@ -4,7 +4,7 @@
  * Created Date: 23.12.2022 13:51:00
  * Author: 3urobeat
  * 
- * Last Modified: 30.12.2022 14:44:34
+ * Last Modified: 30.12.2022 21:54:37
  * Modified By: 3urobeat
  * 
  * Copyright (c) 2022 3urobeat <https://github.com/HerrEurobeat>
@@ -28,3 +28,89 @@ void setupModeSetup();
 void setupModeLoop();
 
 void setupModeWebPage(AsyncWebServerRequest *request);
+
+
+// setupModeWebPage HTML content
+const char webpage[] PROGMEM = R"rawliteral(
+<html>
+    <head>
+        <title>nodemcu-clock Setup Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    
+    <body>
+        <h1>nodemcu-clock Setup Page Hello</h1>
+        
+        <!-- %variable% in value fields are placeholders that get replaced by processor function before sending to client -->
+        <form action="/get" target="hidden-form">
+            <!-- Wifi Settings -->
+            <h3>Wifi Settings</h3>
+            <div id="wifi_networks">
+                <p>Network 1:</p>
+                SSID: <input type="text" name="wifiSSID_input1" value="%wifiSSID_input1%"><br>
+                Password: <input type="text" name="wifiPW_input1" value="%wifiPW_input1%"><br>
+            </div>
+            <br>
+            <input type="button" id="addWifi_button" value="+ Add another network" onclick="addWifiNetworkToDiv()"><br> <!-- Button to add another SSID and PW field -->
+            <br>
+            Setup-Wifi Password: <input type="text" name="setupWifiPW_input" value="%setupWifiPW_input%"><br>
+            <br><br>
+
+            <!-- API Keys -->
+            <h3>API Keys</h3>
+            OpenWeatherMap API Key: <input type="text" name="openweathermaptoken_input" value="%openweathermaptoken_input%"><br>
+            News API Key: <input type="text" name="newsapitoken_input" value="%newsapitoken_input%"><br>
+            <br><br>
+
+            <!-- Localization -->
+            <h3>Localization</h3>
+            Coordinates - Lateral (optional): <input type="text" name="lat_input" value="%lat_input%"><br>
+            Coordinates - Longitudinal (optional): <input type="text" name="lon_input" value="%lon_input%"><br>
+            Date Format: <input type="text" name="dateformat_input" value="%dateformat_input%"><br>
+            Time Format: <input type="text" name="timeformat_input" value="%timeformat_input%"><br>
+            Mini-Clock Format: <input type="text" name="miniClockFormat_input" value="%miniClockFormat_input%"><br>
+            <br><br>
+
+            <!-- System config -->
+            <h3>System Config:</h3>
+            LCD Columns: <input type="number" name="maxcol_input" value="%maxcol_input%"><br>
+            Page Order: <input type="text" name="pageOrder_input" value="%pageOrder_input%"><br>
+            Page Duration: <input type="text" name="showuntil_input" value="%showuntil_input%"><br>
+            Enable Mini-Clock: <input type="checkbox" name="alwaysShowTime_input" checked="%alwaysShowTime_input%"><br>
+            Clock-Weekday-Switch Delay: <input type="number" name="clockWeekdaySwitch_input" value="%clockWeekdaySwitch_input%"><br>
+            <br><br>
+            
+            <!-- Save button -->
+            <input type="submit" value="Save">
+        </form>
+        <iframe style="display:none" name="hidden-form"></iframe>
+    </body>
+
+    <script>
+        // TODO: Add amount of wifi networks specified in config on page load
+        let wifiNetworksDiv      = document.getElementById("wifi_networks");
+        let amountOfWifiNetworks = 2; // TODO
+
+        // TODO: Specify some sort of placeholder array containing placeholder variables for value fields so that processor() can replace them
+
+        for (let i = 2; i <= amountOfWifiNetworks; i++) {
+            wifiNetworksDiv.innerHTML += `
+                <p>Network ${i}:</p>
+                SSID: <input type="text" name="wifiSSID_input${i}" value=""><br>
+                Password: <input type="text" name="wifiPW_input${i}" value=""><br>
+            `
+        }
+
+        // Function that will be called by button onclick
+        function addWifiNetworkToDiv() {
+            amountOfWifiNetworks++;
+
+            wifiNetworksDiv.innerHTML += `
+                <p>Network ${amountOfWifiNetworks}:</p>
+                SSID: <input type="text" name="wifiSSID_input${amountOfWifiNetworks}" value=""><br>
+                Password: <input type="text" name="wifiPW_input${amountOfWifiNetworks}" value=""><br>
+            `
+        }
+    </script>
+</html>
+)rawliteral";
--- src/setupMode/setupModeWebPage.cpp
+++ src/setupMode/setupModeWebPage.cpp
@@ -4,7 +4,7 @@
  * Created Date: 24.12.2022 19:02:04
  * Author: 3urobeat
  * 
- * Last Modified: 30.12.2022 14:57:26
+ * Last Modified: 30.12.2022 21:54:22
  * Modified By: 3urobeat
  * 
  * Copyright (c) 2022 3urobeat <https://github.com/HerrEurobeat>
@@ -17,18 +17,17 @@
 
 #include "setupMode.h"
 
-// TODO: Do this differently
-const char webpage[] = "<html><head><title>nodemcu-clock Setup Page</title></head><body><h1>nodemcu-clock Setup Page</h1><p>Hello World</p></body></html>";
+
+// Processor function to dynamically replace placeholder variables in webpage
+String processor(const String& var) // I need to use String here because the library forces me to... https://github.com/me-no-dev/ESPAsyncWebServer#send-large-webpage-from-progmem-containing-templates-and-extra-headers
+{
+    
+    return String();
+}
+
 
 // Serves the page when user visits webserver
 void setupModeWebPage(AsyncWebServerRequest *request)
 {
-    request->send_P(200, "text/html", webpage);
-}
-
-
-// Handles sending variable data to client
-void setupModeWebPageLoop()
-{
-
+    request->send_P(200, "text/html", webpage, processor); // webpage is defined in header file
 }
