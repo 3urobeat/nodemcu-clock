a1d00631b4f391c7e87a7b626bd8c4df273b281c
3urobeat <tomke@goritz.org>
2025-10-05 15:56:45 +0200

    fix: Fix incorrect backlight pin for ESP32

--- src/helpers/backlight.cpp
+++ src/helpers/backlight.cpp
@@ -4,10 +4,10 @@
  * Created Date: 2024-05-12 10:03:02
  * Author: 3urobeat
  *
- * Last Modified: 2024-05-12 10:30:24
+ * Last Modified: 2025-10-05 15:49:58
  * Modified By: 3urobeat
  *
- * Copyright (c) 2024 3urobeat <https://github.com/3urobeat>
+ * Copyright (c) 2024 - 2025 3urobeat <https://github.com/3urobeat>
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -23,7 +23,7 @@ bool backlightEnabled = false;
 #ifdef ESP8266
     const uint8_t switchPin = D3;
 #elif ESP32
-    const uint8_t switchPin = 19;
+    const uint8_t switchPin = 18;
 #endif
 
 
